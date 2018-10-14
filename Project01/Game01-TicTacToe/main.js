674e5bb0c95035d1997dfc85125ba0f0482a2c48
atheer 10 <atheer.m.070@gmail.com>
2018-10-14 09:18:04 +0300

    first commit

---
+++ Project01/Game01-TicTacToe/main.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function() {
+
+
+//We want to create a function that locks a player choice to the zone 
+// There should be two player/turn
+
+function playerClicks(firstPlayer,secondPlayer) {
+
+    function firstPlayer () { 
+    $(".zone").on("mouseenter"),function (event) {
+$(this).addClass("green");
+$(".zone".on("mouseout"),function (event) {
+    $(this).removeClass ("green");
+});
+
+    }
+    }
+
+}
+
+
+});
